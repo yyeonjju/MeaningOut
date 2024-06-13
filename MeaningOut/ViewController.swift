fb7e02307881186b8d6f4c0500396b843a3029e2
yyeonjju <llilyha123@gmail.com>
2024-06-13 21:23:34 +0900

    Initial Commit

---
+++ MeaningOut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeaningOut
+//
+//  Created by 하연주 on 6/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
